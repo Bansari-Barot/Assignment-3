caa2d51a2011de7d7958a4abc7132d9d2745bbb6
Bansari-Barot <38364791+Bansari-Barot@users.noreply.github.com>
2018-05-11 22:36:45 -0700

    Update update.js

--- update.js
+++ update.js
@@ -1,21 +1,17 @@
-
 //first update query
-result = db.slack.update({ "channels.cname": "general"}, { $push:{"channels.users":{cname:"Snow", email:"sw@bn.com"}} } );
+result = db.slack.update({ "channels.cname": "general"}, { $push:{"channels.users":{uname:"Snow", email:"sw@bn.com"}} } );
 print('Result for "adding one user-snow in the array of users in channel-general"');
 print('Result for "get all the posts, name of user and email by user-bansri in channel-general"');
-while ( result.hasNext() ) {
-printjson( result.next() );
-}
+printjson( result );
+
 
 //second query
-result = db.slack.update({wname:"machine learning", channels:{$ne:"project-3"}}, {$push:{"channels":{cname:"project-3", startDate:new Date("May 11, 2018")}}});
+result1 = db.slack.update({wname:"machine learning", channels:{$ne:"project-3"}}, {$push:{"channels":{cname:"project-3", startDate:new Date("May 11, 2018")}}});
 print('Result for "add one chanel-project 3 if it does not exist in workspace-machine learning"');
-while ( result1.hasNext() ) {
-printjson( result1.next() );
-}
+printjson( result1 );
 
 
 //remove query
 result = db.slack.remove({wname:"cs6820"});
-print('delet the workspace named cs6820');
+print('delete the workspace named cs6820');
 printjson(result);
